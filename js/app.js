fc7ab8f4d35a9cd3b0da785f4cf48396f5e3e5e9
Herdez <abelquantikum@gmail.com>
2018-07-24 09:19:57 -0500

    Activity Javascript Repeat a String

---
+++ js/app.js
@@ -0,0 +1,36 @@
+//confirm an string
+function confirmEnding(str, char) {
+  if (str[str.length - 1] === char) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+console.assert(confirmEnding("Bastian", "n") === true); // true
+console.assert(confirmEnding("Hold on", "o") === false); // false
+
+console.log("Test Good");
+
+//repeat an string
+//Repeat a given string (first argument) num times (second argument). 
+//Return an empty string if num is not a positive number.
+
+
+function repeatStringNumTimes(str, number) {
+  if (typeof number === "number" && number > 0 ) {
+    return str.repeat(number);
+  } else {
+    return '""';
+  }
+}
+
+
+console.assert(repeatStringNumTimes("hello", 5) === "hellohellohellohellohello"); // "hellohellohellohellohello"
+console.assert(repeatStringNumTimes("abc", 3) === "abcabcabc"); // "abcabcabc"
+console.assert(repeatStringNumTimes("abc", "2") === '""'); // ""
+console.assert(repeatStringNumTimes("mr_robot", -1) === '""'); // ""
+console.assert(repeatStringNumTimes("friend?", "5") === '""'); // ""
+
+console.log("Test Good");
